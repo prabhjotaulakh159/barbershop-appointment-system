76f1a4f3d83f3a0ace2d76c0d20f3c6c33d1af9b
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-19 16:56:44 -0400

    configured flask_login to include professional

--- appointment_app/user/auth_config.py
+++ appointment_app/user/auth_config.py
@@ -1,26 +1,57 @@
 from flask_login import LoginManager, UserMixin
 
+
 login_manager = LoginManager()
 
-class User(UserMixin):
-    ''' Extends UserMixin for flask login '''
-    
-    def __init__(self):
-        ''' Constructor '''
-        pass
+
+class Client(UserMixin):
+    ''' Client login configuration '''
+    def __init__(self, client_id, username, password, email, avatar, phone):
+        ''' Constructor with all parameters for a user '''
+        self.client_id = client_id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.avatar = avatar
+        self.phone = phone
 
     def get_id(self):
         ''' Gets the ID of the user in session'''
-        pass
+        return self.client_id
+
+
+class Professional(UserMixin):
+    ''' Professional login configuration '''
+    def __init__(self, professional_id, username, password, email, avatar,
+                 phone, pay_rate, speciality):
+        self.professional_id = professional_id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.avatar = avatar
+        self.phone = phone
+        self.pay_rate = pay_rate
+        self.specialty = speciality
+
 
 login_manager = LoginManager()
 
+
 @login_manager.user_loader
-def load_user(user_id):
-    ''' Loads the user from session '''
-    pass
+def load_client(client_id, username, password, email, avatar, phone):
+    ''' Loads the client from the session '''
+    return Client(client_id, username, password, email, avatar, phone)
+
+
+@login_manager.user_loader
+def load_professional(professional_id, username, password, email, avatar,
+                      phone, pay_rate, speciality):
+    ''' Loads the professional from the session '''
+    return Professional(professional_id, username, password, email, avatar,
+                        phone, pay_rate, speciality)
+
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    ''' Redirects to some page if not authorized '''
+    ''' Redirects to a login page if user accesses an unauthorized page '''
     pass
