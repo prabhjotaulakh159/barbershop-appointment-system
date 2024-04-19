17fafaa4e5fbb75b20ebaf8c49bbaf2740b5b059
TommyTr <TommyTran96@hotmail.com>
2024-04-19 11:32:58 -0400

    Added add client & get client methods in database.py

--- appointment_app/user/routes.py
+++ appointment_app/user/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect
+from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_bcrypt import Bcrypt
 from appointment_app.qdb.database import Database
 from appointment_app.user.client import Member
@@ -8,6 +8,7 @@ from appointment_app.user.forms import RegisterMemberForm
 user = Blueprint('user', __name__, template_folder="templates",
                  static_folder="static")
 
+db = Database()
 
 @user.route("/register", methods=["GET", "POST"])
 def register():
@@ -16,8 +17,8 @@ def register():
     context_data = {"page_title": "Registration"}
     if form.validate_on_submit():
         username = form.username.data
-        user_exist = db.get_client(username)
-        if user_exist:
+        client_exist = db.get_client(username)
+        if client_exist:
             flash(f'{username} already in db. Choose another username', 'error')
         else:
             b = Bcrypt()
@@ -30,4 +31,5 @@ def register():
             db.add_client(username, password, email, avatar, phone)
             flash(f'Welcome {username} you are now a client', 'success')
             return redirect(url_for('user.login'))
+        
     return render_template("registration.html", context=context_data, form=form)
