4c5e915c9ea0007838bd85943c0aa3373f1d568b
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-19 17:01:20 -0400

    configured base.html

--- appointment_app/main/routes.py
+++ appointment_app/main/routes.py
@@ -1,15 +1,10 @@
-'''import flask and its methods'''
 from flask import Blueprint, render_template
-from appointment_app.qdb.database import Database
 
-main = Blueprint('main', __name__, template_folder="templates",
-                 static_folder="static")
+main = Blueprint('main', __name__, template_folder="/templates")
 
 
 @main.route("/")
-@main.route("/home")
 def home_view():
-    '''function rendering for the route / and /home'''
-    context_data = {"page_title": "home"}
-    return render_template("home.html", context=context_data)
+    ''' Renders the homepage '''
+    return render_template("home.html")
 
