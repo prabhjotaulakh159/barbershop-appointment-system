3607336c5d9e64306b11e0e1d440b4a7947f3e51
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-23 15:19:16 -0400

    Changed user mixin class

--- appointment_app/user/auth_config.py
+++ appointment_app/user/auth_config.py
@@ -6,13 +6,21 @@ login_manager = LoginManager()
 
 class User(UserMixin):
     ''' Base class representing a user '''
-
-    def __init__(self, username, password, email, avatar, phone):
-        self.username = username
-        self.password = password
+    def __init__(self, user_id, is_active, access_level, user_type, user_name, 
+                 pass_word, email, avatar, phone, address, age, pay_rate, specialty):
+        self.user_id = user_id
+        self.is_active = is_active
+        self.access_level = access_level
+        self.user_type = user_type
+        self.user_name = user_name
+        self.pass_word = pass_word
         self.email = email
         self.avatar = avatar
         self.phone = phone
+        self.address = address
+        self.age = age
+        self.pay_rate = pay_rate
+        self.specialty = specialty
 
 class Client(User):
     ''' Class representing a client '''
--- appointment_app/user/forms.py
+++ appointment_app/user/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, SubmitField, FloatField,SelectField, NumberField
+from wtforms import EmailField, PasswordField, StringField, SubmitField, FloatField,SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
@@ -11,7 +11,7 @@ class RegisterClientForm(FlaskForm):
     email = EmailField ('Email', validators=[DataRequired(),])
     phone = StringField("Phone", validators=[DataRequired(), Length(min=10,max=12)])
     user_type = SelectField("Type", choices=[('Member', 'Member'), ('Professional', 'Professional')])
-    pay_rate = NumberField(min=1, max=100)
+    pay_rate = FloatField(min=1, max=100)
     specialty = specialty = SelectField("Specialty", choices=[
         ('Hair Stylist', 'Hair Stylist'),
         ('Colorist', 'Colorist'),
@@ -22,6 +22,6 @@ class RegisterClientForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2,max=25)])
-    email = EmailField ('Email', validators=[DataRequired(),])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Length(min=2)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=5)])
     submit = SubmitField("Login")
