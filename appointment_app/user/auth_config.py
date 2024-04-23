88e18bbbc06abbc8840888dc95cadcab9d9b253d
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-23 15:23:53 -0400

    Changed configuration for flask-login

--- appointment_app/user/auth_config.py
+++ appointment_app/user/auth_config.py
@@ -21,44 +21,19 @@ class User(UserMixin):
         self.age = age
         self.pay_rate = pay_rate
         self.specialty = specialty
-
-class Client(User):
-    ''' Class representing a client '''
-    def __init__(self, client_id, username, password, email, avatar, phone):
-        super().__init__(username, password, email, avatar, phone)
-        self.client_id = client_id
-    
+        
     def get_id(self):
-        ''' Gets the ID of the user in session'''
-        return self.client_id
-
-class Professional(User):
-    ''' Professional login configuration '''
-
-    def __init__(self, professional_id, username, password, email, avatar,
-                 phone, pay_rate, speciality):
-        super().__init__(username,password,email,avatar,phone)
-        self.professional_id = professional_id
-        self.payrate = pay_rate
-        self.specialty = speciality
+        return self.user_name
 
 
 login_manager = LoginManager()
 
-
 @login_manager.user_loader
-def load_client(client_id, username, password, email, avatar, phone):
+def load_user(user_id, is_active, access_level, user_type, user_name, 
+                pass_word, email, avatar, phone, address, age, pay_rate, specialty):
     ''' Loads the client from the session '''
-    return Client(client_id, username, password, email, avatar, phone)
-
-
-@login_manager.user_loader
-def load_professional(professional_id, username, password, email, avatar,
-                      phone, pay_rate, speciality):
-    ''' Loads the professional from the session '''
-    return Professional(professional_id, username, password, email, avatar,
-                        phone, pay_rate, speciality)
-
+    return User(user_id, is_active, access_level, user_type, user_name, pass_word, email, avatar,
+                phone, address, age, pay_rate, specialty)
 
 @login_manager.unauthorized_handler
 def unauthorized():
