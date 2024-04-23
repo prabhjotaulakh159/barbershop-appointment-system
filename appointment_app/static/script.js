46536b2b54fb8c736343e37f620758bf542fd260
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-23 15:35:30 -0400

    Added javascript to render professional fields and added user_type to register form

---
+++ appointment_app/static/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    const userTypeInput = document.getElementById('user-type');
+    const payrateInput = document.getElementById('pay-rate');
+    const specialityInput = document.getElementById('speciality');
+
+    userTypeInput.addEventListener('click', renderProfessionalReservedFields);
+
+    const renderProfessionalReservedFields = (e) => {
+        if (e.target.value == 'Member') {
+            payrateInput.style.display = 'none';
+            specialityInput.style.display = 'none';
+        } else if (e.target.value == 'Professional') {
+            payrateInput.style.display = 'block';
+            specialityInput.style.display = 'block';
+        } else {
+            throw new Error('Invalid value found for user type !');
+        }
+    };
+});
