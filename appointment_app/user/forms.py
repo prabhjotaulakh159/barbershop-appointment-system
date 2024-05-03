a6da308b4dd1785e943b0115cd45e688b8e5bf78
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-03 18:22:44 -0400

    some refactoring

--- appointment_app/user/forms.py
+++ appointment_app/user/forms.py
@@ -1,76 +1,36 @@
 ''' Contains form for authentication '''
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
-from wtforms import (EmailField, PasswordField, StringField, SubmitField,
-                     SelectField, FileField, FloatField, IntegerField)
-from wtforms.validators import (DataRequired, Length, EqualTo, NumberRange,
-                                Regexp, Optional)
+from wtforms import EmailField, PasswordField, StringField, SubmitField, SelectField, FileField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo, NumberRange, Regexp, Optional
 
 PHONE_REGEX = '^[0-9]{3,3}-[0-9]{3,3}-[0-9]{4,4}$'
 
-
 class RegisterUserForm(FlaskForm):
-    ''' class representing registration form '''
-    username = StringField("Username", validators=[DataRequired(),
-                                                   Length(min=2, max=25)])
-
+    ''' Class representing registration form '''
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=25)])
     password = PasswordField("Password", validators=[DataRequired()])
-
-    confirm_password = PasswordField("Retype password",
-                                     validators=[DataRequired(),
-                                                 EqualTo("password")])
-
+    confirm_password = PasswordField("Retype password", validators=[DataRequired(), EqualTo("password")])
     email = EmailField('Email', validators=[DataRequired()])
-
-    phone = StringField("Phone",
-                        validators=[DataRequired(),
-                                    Regexp(PHONE_REGEX,
-                                    message='Phone number not valid')])
-
-    user_type = SelectField("Type",
-                            choices=[('Member', 'Member'),
-                                     ('Professional', 'Professional')])
-
+    phone = StringField("Phone", validators=[DataRequired(), Regexp(PHONE_REGEX, message='Phone number not valid')])
+    user_type = SelectField("Type", choices=[('Member', 'Member'), ('Professional', 'Professional')])
     age = IntegerField(validators=[NumberRange(min=0, max=100)])
-
-    address = StringField("Address", validators=[DataRequired(),
-                                                 Length(min=2, max=25)])
-
+    address = StringField("Address", validators=[DataRequired(), Length(min=2, max=25)])
     pay_rate = FloatField(validators=[Optional(), NumberRange(min=1, max=100)])
-
-    avatar = FileField('avatar', validators=[FileAllowed(['jpg', 'png'])])
-
-    specialty = SelectField("Specialty", choices=[
-        ('Hair Stylist', 'Hair Stylist'),
-        ('Colorist', 'Colorist'),
-        ('Barber', 'Barber'),
-        ('Other', 'Other')
-    ])
+    avatar = FileField('Avatar', validators=[FileAllowed(['jpg', 'png'])])
+    specialty = SelectField("Specialty", choices=[('Hair Stylist', 'Hair Stylist'), ('Colorist', 'Colorist'), ('Barber', 'Barber'), ('Other', 'Other')])
     submit = SubmitField("Register")
-
 
 class LoginForm(FlaskForm):
     ''' Form for login '''
-    username = StringField("Username", validators=[DataRequired(),
-                                                   Length(min=2, max=25)])
-
-    password = PasswordField("Password", validators=[DataRequired(),
-                                                     Length(min=5)])
-
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=25)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=5)])
     submit = SubmitField("Login")
 
 
 class ChangePasswordForm(FlaskForm):
     ''' Form to change passwords '''
-    old_password = PasswordField("Password", validators=[DataRequired(),
-                                                         Length(min=5)])
-
-    new_password = PasswordField("Password", validators=[DataRequired(),
-                                                         Length(min=5)])
-
-    confirm_new_password = PasswordField("Password",
-                                         validators=[DataRequired(),
-                                                     Length(min=5),
-                                                     EqualTo("new_password")])
-
-    submit = SubmitField("Change password")
+    old_password = PasswordField("Old Password", validators=[DataRequired(), Length(min=5)])
+    new_password = PasswordField("New Password", validators=[DataRequired(), Length(min=5)])
+    confirm_new_password = PasswordField("Confirm New Password", validators=[DataRequired(), Length(min=5), EqualTo("new_password")])
+    submit = SubmitField("Change Password")
--- appointment_app/user/routes.py
+++ appointment_app/user/routes.py
@@ -5,9 +5,7 @@ from flask_login import login_user, current_user, login_required, logout_user
 from appointment_app.user.auth_config import User
 from appointment_app.user.utils import save_file
 from appointment_app.qdb.database import db
-from appointment_app.user.forms import (RegisterUserForm,
-                                        LoginForm,
-                                        ChangePasswordForm)
+from appointment_app.user.forms import RegisterUserForm, LoginForm, ChangePasswordForm
 
 
 user = Blueprint('user', __name__, template_folder="templates")
@@ -24,26 +22,11 @@ def login():
         if not userdb:
             flash("You provided invalid credentials", "error")
             return redirect(url_for('user.login'))
-        user_id = userdb[0]
-        is_enabled = userdb[1]
-        access_level = userdb[2]
-        user_type = userdb[3]
-        user_name = userdb[4]
-        pass_word = userdb[5]
-        email = userdb[6]
-        avatar = userdb[7]
-        phone = userdb[8]
-        address = userdb[9]
-        age = userdb[10]
-        pay_rate = userdb[11]
-        specialty = userdb[12]
         bcrypt = Bcrypt()
-        if not bcrypt.check_password_hash(pass_word, form.password.data):
+        if not bcrypt.check_password_hash(userdb[5], form.password.data):
             flash("You provided invalid credentials", "error")
             return redirect(url_for('user.login'))
-        userdb = User(user_id, is_enabled, access_level, user_type,
-                      user_name, pass_word, email, avatar, phone, address,
-                      age, pay_rate, specialty)
+        userdb = User(userdb[0], userdb[1], userdb[2], userdb[3], userdb[4], userdb[5], userdb[6], userdb[7], userdb[8], userdb[9], userdb[10], userdb[11], userdb[12])
         login_user(userdb)
         flash("You have sucessfully logged in !", "success")
         return redirect(url_for('main.home'))
@@ -66,31 +49,19 @@ def register():
         return redirect(url_for('main.home'))
     form = RegisterUserForm()
     if form.validate_on_submit():
-        username = form.username.data
-        user_exist = db.get_user(f"WHERE user_name = '{username}'")
+        user_exist = db.get_user(f"WHERE user_name = '{form.username.data}'")
         if user_exist:
-            flash(f'{username} takne, choose another username', 'error')
+            flash(f'{form.data.username} taken, choose another username', 'error')
             return redirect(url_for('user.register'))
-        b = Bcrypt()
-        usertype = form.user_type.data
-        username = form.username.data
-        password = b.generate_password_hash(form.password.data).decode('utf-8')
-        email = form.email.data
+        bcrypt = Bcrypt()
         avatar = '/images/avatar.png'
         if form.avatar.data:
             avatar = '/images/' + save_file(form.avatar.data)
-        phone = form.phone.data
-        address = form.address.data
-        age = form.age.data
-        if usertype == "Member":
-            payrate = None
-            speciality = None
-        else:
-            payrate = form.pay_rate.data
-            speciality = form.specialty.data
-        db.add_user(usertype, username, password, email,
-                    avatar, phone, address, age, payrate, speciality)
-        flash(f'Welcome {username} you are now a user', 'success')
+        if form.user_type.data == "Member":
+            form.payrate.data = None
+            form.specialty.data = None
+        db.add_user(form.user_type.data, form.username.data, bcrypt.generate_password_hash(form.password.data).decode("utf-8"), form.email.data, avatar, form.phone.data, form.address.data, form.age.data, form.pay_rate.data, form.specialty.data)
+        flash(f'Welcome {form.username.da} you are now a user', 'success')
         return redirect(url_for('user.login'))
     return render_template("register.html", form=form)
 
@@ -113,21 +84,17 @@ def update_profile(user_id):
         form.specialty.data = current_user.specialty
     else:
         if form.username.data != current_user.user_name:
-            user_already_exists = db.get_user(
-                f"WHERE user_name = '{form.username.data}'")
+            user_already_exists = db.get_user(f"WHERE user_name = '{form.username.data}'")
             if user_already_exists:
                 flash("Username already taken !", "error")
                 return redirect(url_for('user.login'))
         new_avatar = current_user.avatar
         if form.avatar.data:
             new_avatar = '/images/' + save_file(form.avatar.data)
-        db.update_user(user_id, form.username.data, form.email.data,
-                       new_avatar, form.phone.data, form.address.data,
-                       form.age.data, form.pay_rate.data, form.specialty.data)
+        db.update_user(user_id, form.username.data, form.email.data, new_avatar, form.phone.data, form.address.data, form.age.data, form.pay_rate.data, form.specialty.data)
         flash("You have successfully upated your profile !", "success")
         return redirect(url_for('main.home'))
-    return render_template('update-profile.html', current_user=current_user,
-                           form=form)
+    return render_template('update-profile.html', current_user=current_user, form=form)
 
 
 @user.route("/change-password/<int:user_id>", methods=['GET', 'POST'])
@@ -142,11 +109,9 @@ def change_password(user_id):
         bcrypt = Bcrypt()
         if not bcrypt.check_password_hash(userdb[5], form.old_password.data):
             flash("You have provided invalid credentials !", "error")
-            return redirect(url_for('user.change_password',
-                                    user_id=current_user.user_id))
+            return redirect(url_for('user.change_password', user_id=current_user.user_id))
         new_password = form.confirm_new_password.data
-        encrypted_new_password = bcrypt.generate_password_hash(new_password)
-        encrypted_new_password = encrypted_new_password.decode('utf-8')
+        encrypted_new_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
         db.change_password(user_id, encrypted_new_password)
         logout_user()
         flash("You have successfully updated your password !", "success")
@@ -160,5 +125,4 @@ def all_users():
     ''' Gets all users from the db'''
     form = RegisterUserForm()
     users = db.get_all_users()
-    return render_template('all-users.html', users=users,
-                           form=form)
+    return render_template('all-users.html', users=users, form=form)
