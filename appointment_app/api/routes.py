b050e06f19d5d99d0c14b9c6bf247c24184b5b1d
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-06 15:17:55 -0400

    Finished 1 resource for appointment

--- appointment_app/api/routes.py
+++ appointment_app/api/routes.py
@@ -1,7 +1,8 @@
 """api for application"""
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 from flask_restful import Api, Resource
 from appointment_app.qdb.database import db
+
 
 api_blueprint = Blueprint("api", __name__)
 api = Api(api_blueprint)
@@ -16,18 +17,39 @@ class Appointments(Resource):
     
     def post(self):
         '''post a new appointment'''
-        data = request.json()
-        status = data['status']
-        date_appointment = data['date_appointment']
-        slot = data['slot']
-        venue = data['venue']
-        client_id = data['client_id']
-        professional_id = data['professional_id']
-        service_id = data['service_id']
-        db.add_appointment(status=status,
-                           date_appointment=date_appointment,
-                           slot=slot,
-                           venue=venue,
-                           client_id=client_id,
-                           professional_id=professional_id,
-                           service_id=service_id)
+        # get data
+        data = request.json
+        
+        # if the request is empty
+        if not data:
+            abort(code=400, message="Missing data")
+        
+        # check all fields to make an appointments object are there
+        required_fields = ['status', 'date_appointment', 'slot', 'venue', 'client_id', 'professional_id', 'service_id']
+        for field in required_fields:
+            if field not in data:
+                abort(code=400, message=f"Missing field: {field}")
+        
+        # check if client exists
+        client = db.get_user(f"WHERE client_id = {data['client_id']}")
+        if not client:
+            abort(code=400, message="Client ID not found")
+            
+        # check if professional exists
+        prof = db.get_user(f"WHERE professional_id = {data['professional_id']}")
+        if not prof:
+            abort(code=400, message="Professional ID not found")
+            
+        # check if service exists
+        service = db.get_service(f"WHERE service_id = {data['service_id']}")
+        if not service:
+            abort(code=400, message="Service not found")
+        
+        # make an appointment model
+        db.add_appointment(status=data['status'],
+                           date_appointment=data['date_appointment'],
+                           slot=data['slot'],
+                           venue=data['venue'],
+                           client_id=data['client_id'],
+                           professional_id=data['professional_id'],
+                           service_id=data['service_id'])
