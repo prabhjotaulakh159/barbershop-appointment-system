a0eb474414cfe5627c9df87da77884f3d95b16f9
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-07 14:27:38 -0400

    Added not-found and internal server error pages

--- appointment_app/__init__.py
+++ appointment_app/__init__.py
@@ -1,18 +1,18 @@
-from flask import Flask
+''' Gets a flask app instance '''
+from flask import Flask, render_template
 from appointment_app.config import ConfigDev
+from appointment_app.main.routes import main
+from appointment_app.administration.routes import administration
+from appointment_app.appointment.routes import appointment
+from appointment_app.user.routes import user
+from appointment_app.report.routes import report
+from appointment_app.user.auth_config import login_manager
+from appointment_app.api.routes import api_blueprint
 
 def create_app(config=ConfigDev):
     ''' Creates an instance of our application with a configuration '''
     app = Flask(__name__)
     app.config.from_object(config)
-
-    from appointment_app.main.routes import main
-    from appointment_app.administration.routes import administration
-    from appointment_app.appointment.routes import appointment
-    from appointment_app.user.routes import user
-    from appointment_app.report.routes import report
-    from appointment_app.user.auth_config import login_manager
-    from appointment_app.api.routes import api_blueprint
 
     app.register_blueprint(main)
     app.register_blueprint(administration)
@@ -22,5 +22,13 @@ def create_app(config=ConfigDev):
     app.register_blueprint(api_blueprint)
     
     login_manager.init_app(app)
+    
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template('not-found.html')
+    
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('internal-server-error.html')
     
     return app
