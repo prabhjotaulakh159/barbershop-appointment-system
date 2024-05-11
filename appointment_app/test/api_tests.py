9df4bc1676d36565418ac356abe34f4e2c411b28
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-11 00:16:23 -0400

    Created temporary tables

--- appointment_app/test/api_tests.py
+++ appointment_app/test/api_tests.py
@@ -4,3 +4,23 @@ import unittest
 
 class ApiTests(unittest.TestCase):
     '''Unit tests for api'''
+    def setup(self):
+        '''This should create a temporary database table'''
+        
+    def destroy(self):
+        '''This should destroy the temporary database table'''
+        
+    def get_all_appointments(self):
+        '''Test getting all appointments'''
+        
+    def post_one_appointment(self):
+        '''Test creating a new appointment'''
+        
+    def get_one_appointment(self, appointment_id):
+        '''Test getting a single appointment'''
+        
+    def update_one_appointment(self, appointment_id):
+        '''Test updating a single appointment'''
+        
+    def delete_one_appointment(self, appointment_id):
+        '''Test deleting a single appointment'''
