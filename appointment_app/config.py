c4ab6555811cb2894997eab06fb338afd85631d4
TommaiT <tommytran96@hotmail.com>
2024-05-13 21:18:30 -0400

    Changed the secret key back to os.environ.get

--- appointment_app/config.py
+++ appointment_app/config.py
@@ -6,7 +6,7 @@ env.load_dotenv()
 
 class ConfigDev:
     ''' Condifuration for dev env'''
-    SECRET_KEY = '413de6338d72b9071370af8534c6213b' #os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = True
     APP_ENV = "develop"
 
