186250fb3d3fac65222717f1faad4fc795ac268b
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-16 13:26:25 -0400

    gunicorn config

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from appointment_app import create_app
-from appointment_app.config import ConfigDev
+from appointment_app.config import ConfigProd
 
 
 if __name__ == "__main__":
-    app = create_app(ConfigDev())
+    app = create_app(ConfigProd())
     app.run(port=5009, debug=True)
+else:
+    gunicorn_app = create_app(config=ConfigProd())
